8f93b79f40c31479da65cbb5a1ae3c8d9c8e9303
prashant chavan <chavanmadhav880@gmail.com>
2021-11-28 22:30:58 +0530

    basics og javascipt

---
+++ day 7 javascript/app.js
@@ -0,0 +1,22 @@
+console.log("hallow world");
+
+//variable in javascipt
+
+/*let – is a modern variable declaration.
+var – is an old-school variable declaration. Normally we don’t use it at all, but we’ll cover subtle differences from let in the chapter The old "var", just in case you need them.
+const – is like let, but the value of the variable can’t be changed.*/
+
+var x =10
+var y=20
+console.log(x+y);
+
+let name="prashant"
+let n=90
+console.log(name + n);
+
+
+const a=20 
+console.log(a);
+// using cost we cannot change the value
+a=30
+console.log(a);
---
+++ day 7 javascript/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const x = 'x'
+const y = 'y'
+console.log(x, y)
