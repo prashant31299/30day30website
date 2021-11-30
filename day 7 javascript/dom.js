82ee37fac35214b37defca6086f306706ba4bf89
prashant chavan <chavanmadhav880@gmail.com>
2021-11-30 21:08:26 +0530

    learn dom manipulation

--- day 7 javascript/dom.js
+++ day 7 javascript/dom.js
@@ -1,3 +1,8 @@
-console.log("hallow worldsfsdfs");
+document.getElementsByTagName("h1")[0].innerHTML="good morning"
+document.getElementsByTagName("p")[0].innerHTML="hows daty"
+document.getElementsByTagName("p")[1].innerHTML="what do you do "
+document.getElementById("h1").innerHTML="L.A"
+document.getElementById("h3").innerHTML="standford"
+document.getElementsByClassName("intro")[0].innerHTML="im devloper what wronge with  you"
 
-document.getElementById("demo").innerHTML = "Hello World!";
+
