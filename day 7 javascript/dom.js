d94d8750bde0da0a9285218a5c74275ff0fa4d7c
prashant chavan <chavanmadhav880@gmail.com>
2021-11-30 22:36:07 +0530

    learn dom manipulation

--- day 7 javascript/dom.js
+++ day 7 javascript/dom.js
@@ -3,6 +3,19 @@ document.getElementsByTagName("p")[0].innerHTML="hows daty"
 document.getElementsByTagName("p")[1].innerHTML="what do you do "
 document.getElementById("h1").innerHTML="L.A"
 document.getElementById("h3").innerHTML="standford"
-document.getElementsByClassName("intro")[0].innerHTML="im devloper what wronge with  you"
+document.getElementsByClassName("intro1")[0].innerHTML="im devloper what wronge with  you"
+
+// learning querty selector 
+document.querySelector("h2").innerHTML="changing the query "
+document.querySelectorAll('p.intro')[0].innerHTML="first p changes"
+document.querySelectorAll("p.intro")[1].innerHTML="second p changes"
+document.querySelectorAll("p.intro")[2].innerHTML="third p changes"
 
 
+
+document.querySelector("h1").style.color="red";
+
+
+function changetext(id){
+    id.innerHTML="oops!"
+}
