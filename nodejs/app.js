cbc812b1410e3effde5629b0aca29417f082ebd6
prashant chavan <chavanmadhav880@gmail.com>
2021-12-06 19:23:56 +0530

    change css variable

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,7 +1,3 @@
-var http = require('http');
+const fs =require("fs");
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
+fs.copyFileSync("1.txt","2.txt")
