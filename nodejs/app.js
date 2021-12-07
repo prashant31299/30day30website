1a2b32196207585c9c4f3d3edaa13ae735c6c1bf
prashant chavan <chavanmadhav880@gmail.com>
2021-12-07 17:58:54 +0530

    crating calculator app using node js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,6 +1,18 @@
 express = require("express")
 app =express();
 
-app.listen(3000,()=>{
-    console.log("server started at port 8000");
+app.get("/",(req,res)=>{
+    res.send("hallow wd dsafadaorld ");
 });
+app.get("/contact", (req,res)=>{
+    res.send("this is a my contact page")
+})
+app.get("/about", (req,res)=>{
+    res.send("<h1>hey im prashant chavan im web devloper</h1>")
+})
+app.get("/work", (req,res)=>{
+    res.send("<h1>now im homeless</h1>")
+})
+app.listen(3000,()=>{
+    console.log("server  gsstarted at port 8000");
+}); 
